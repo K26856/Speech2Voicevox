313c578b46981b46d31440556ff7a973cbe78b7a
K26856 <0delta0alpha@gmail.com>
2022-04-20 22:06:54 +0900

    add program

---
+++ Speech2Voicevox/MainPage.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using Windows.Media.Devices;
+using Windows.Devices.Enumeration;
+using System.Collections.Generic;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace Speech2Voicevox
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private const int MAX_LOG_ROW = 50;
+        private const int MAX_CHARA = 20;
+        private DeviceInformation selectedDevice = (DeviceInformation)null;
+        private int selectedChara = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_loaded;
+        }
+
+        private void MainPage_loaded(object sender, RoutedEventArgs args)
+        {
+            audioSelectorBoxInitialized();
+            charaSelectorBoxInitializedAsync();
+            addLogToTextBox("[SystemMessage] Main Page Loaded");
+            wv2.CoreWebView2Initialized += Wv2_Initialized;
+            wv2.EnsureCoreWebView2Async();
+        }
+
+
+        // ---------------------------------------------------------
+        // 再生デバイス選択ボックスのコントロールまわり
+        // ---------------------------------------------------------
+        private async void audioSelectorBoxInitialized()
+        {
+            string audioSelector = MediaDevice.GetAudioRenderSelector();
+            var outputDevices = await DeviceInformation.FindAllAsync(audioSelector);
+            foreach (var device in outputDevices)
+            {
+                var deviceItem = new ComboBoxItem();
+                deviceItem.Content = device.Name;
+                deviceItem.Tag = device;
+                audioSelectorBox.Items.Add(deviceItem);
+            }
+        }
+
+        private void audioSelectorBoxChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceInformation selectedDevice = (DeviceInformation)((ComboBoxItem) audioSelectorBox.SelectedItem).Tag;
+            if (selectedDevice != null)
+            {
+                this.selectedDevice = selectedDevice;
+            }
+        }
+
+
+        // ---------------------------------------------------------
+        // キャラクター選択ボックスのコントロールまわり
+        // ---------------------------------------------------------
+        private async void charaSelectorBoxInitializedAsync()
+        {
+            charaSelectorBox.DisplayMemberPath = "name";
+            List<VoicevoxSpeaker> speakers = await VoicevoxApi.getSpeakers();
+            foreach(VoicevoxSpeaker speaker in speakers)
+            {
+                charaSelectorBox.Items.Add(speaker);
+            }
+        }
+
+        private void charaSelectorBoxChanged(object sender, SelectionChangedEventArgs e)
+        {
+            VoicevoxSpeaker selected = (VoicevoxSpeaker)charaSelectorBox.SelectedItem;
+            if(selected.id > -1)
+            {
+                this.selectedChara = selected.id;
+            }
+        }
+
+
+        // ----------------------------------------------------------------------------------
+        // WebView2 のコントロールまわり
+        // ----------------------------------------------------------------------------------
+        private void Wv2_Initialized(object sender, CoreWebView2InitializedEventArgs args)
+        {
+            addLogToTextBox("[SystemMessage] Web View Initialized");
+            string currentDir = Environment.CurrentDirectory;
+            Uri uri = new Uri($"{currentDir}/index.html");
+            wv2.CoreWebView2.Navigate(uri.AbsoluteUri);
+            wv2.CoreWebView2.WebMessageReceived += Wv2_WevMessageReceived;
+            wv2.CoreWebView2.PermissionRequested += Wv2_PermissionRequested;
+        }
+
+        private void Wv2_WevMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            string text = args.TryGetWebMessageAsString();
+            addLogToTextBox("[SpeechRecognition] " + text);
+            VoicevoxApi.playAudio(text, selectedChara, selectedDevice);
+        }
+
+        private void Wv2_PermissionRequested(object sender, CoreWebView2PermissionRequestedEventArgs args)
+        {
+            if( args.PermissionKind == CoreWebView2PermissionKind.Microphone)
+            {
+                args.State = CoreWebView2PermissionState.Allow;
+            }
+        }
+
+
+
+        //---------------------------------------------------------------------------
+        // TextBoxのコントロールまわり
+        //---------------------------------------------------------------------------
+        private void addLogToTextBox(String log)
+        {
+            DateTime now = DateTime.Now;
+            string beforeText = textBlock.Text;
+            string afterText = now.ToString("yyyy/MM/dd HH:mm:ss") + "  :  " + log + "\r\n" + beforeText;
+
+            int row = 0;
+            int cutPos = 0;
+            for(cutPos=0; cutPos<afterText.Length; cutPos++)
+            {
+                if (row >= MAX_LOG_ROW) break;
+                if (afterText[cutPos] == '\n') row++ ;
+            }
+            afterText = afterText.Substring(0, cutPos);
+
+            textBlock.Text = afterText;
+        }
+    }
+}
---
+++ Speech2Voicevox/VoicevoxApi.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using Windows.Media.Playback;
+using Windows.Media.Core;
+using Windows.Devices.Enumeration;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Speech2Voicevox
+{
+
+    public class VoicevoxSpeaker
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    internal class VoicevoxApi
+    {
+
+        //---------------------------------------------------
+        // Json シリアライズ用のクラス定義
+        //---------------------------------------------------
+        [DataContract]
+        private class Style
+        {
+            [DataMember]
+            public string id { get; set; }
+            [DataMember]
+            public string name { get; set; }
+        }
+
+        [DataContract]
+        private class Speaker
+        {
+            [DataMember]
+            public string name { get; set; }
+            [DataMember]
+            public string speaker_uuid { get; set; }
+            [DataMember]
+            public List<Style> styles { get; set; }
+        }
+
+        //-------------------------------------
+        // エンドポイント
+        //-------------------------------------
+        private static string baseUrl = "http://localhost:50021/";
+
+
+        //-------------------------------------
+        // 使える音声をリストアップ化して返却する非同期関数
+        //-------------------------------------
+        public static async Task<List<VoicevoxSpeaker>> getSpeakers()
+        {
+            var url = baseUrl + "speakers";
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(List<Speaker>));
+                List<Speaker> speakers = serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as List<Speaker>;
+                List<VoicevoxSpeaker> result = new List<VoicevoxSpeaker>();
+                foreach(Speaker speaker in speakers)
+                {
+                    foreach(Style style in speaker.styles)
+                    {
+                        result.Add(new VoicevoxSpeaker()
+                        {
+                            id = int.Parse(style.id),
+                            name = speaker.name + "(" + style.name +")",
+                        });
+                    }
+                }
+                result.Sort((a, b) => a.name.CompareTo(b.name));
+                return result;
+            }
+        }
+
+        // ------------------------------------------
+        // wavファイルを生成し選択したaudioから流す非同期関数
+        // ------------------------------------------
+        private static async void makeNplayWavAudio(int speaker, string body, DeviceInformation device)
+        {
+            var url = baseUrl + "synthesis";
+            url += "?speaker=" + Uri.EscapeDataString(speaker.ToString());
+            url += "&enable_interrogative_upspeak=true";
+
+            var content = new StringContent(body, Encoding.UTF8, @"application/json");
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, content);
+                var mediaPlayer = new MediaPlayer();
+                mediaPlayer.Source = MediaSource.CreateFromStream((await response.Content.ReadAsStreamAsync()).AsRandomAccessStream(), "audio/wav");
+                if (device != null)
+                {
+                    mediaPlayer.AudioDevice = device;
+                }
+                mediaPlayer.Play();
+            }
+        }
+
+        //-----------------------------------------------------
+        // 選択したデバイスから、選択したキャラクターのボイスで、textを読み上げてくれる非同期関数
+        //-----------------------------------------------------
+        public static async void playAudio(string text, int speaker, DeviceInformation device)
+        {
+            var url = baseUrl + "audio_query";
+            url += "?text=" + Uri.EscapeDataString(text);
+            url += "&speaker=" + Uri.EscapeDataString(speaker.ToString());
+
+            var content = new FormUrlEncodedContent(new Dictionary<String, String>(){});
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, content);
+                var result = await response.Content.ReadAsStringAsync();
+                makeNplayWavAudio(speaker, result, device);
+            }
+        }
+    }
+}
